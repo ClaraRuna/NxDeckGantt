ad82cb4e58b133274ceeb09dcb8c859b4d689a3a
Runa Schlör <cr.schloer@posteo.de>
2024-07-24 10:54:23 +0200

    TASK: Fix translations for chrome and safari

--- private/translations.js
+++ private/translations.js
@@ -1,20 +1,28 @@
 export function translate(key, lang) {
-    let translations = {
-        en: {
-            wrongPassword: "Could not log you in, are your username and password correct?",
-            wrongUri: "Cannot connect to nextcloud server. Please check configured URI: ",
-            loginText: "Please log in to give this app access to your Nextcloud Account.",
-        },
-        de: {
-            wrongPassword:
-                "Der Login war nicht möglich. Bitte Benutzername und Passwort prüfen.",
-            wrongUri:
-                "Verbindung mit dem Nextcloud Server nicht möglich. Bitte die konfigurierte URI prüfen: ",
-            loginText:
-                "Bitte einloggen, um Zugang zum Nextcloud Account zu ermöglichen.",
-        },
-    }
-    return translations[lang][key] ||
-        translations["en"][key] ||
-        "translation error";
-};
+  let translations = {
+    en: {
+      wrongPassword:
+        "Could not log you in, are your username and password correct?",
+      wrongUri:
+        "Cannot connect to nextcloud server. Please check configured URI: ",
+      loginText:
+        "Please log in to give this app access to your Nextcloud Account.",
+    },
+    de: {
+      wrongPassword:
+        "Der Login war nicht möglich. Bitte Benutzername und Passwort prüfen.",
+      wrongUri:
+        "Verbindung mit dem Nextcloud Server nicht möglich. Bitte die konfigurierte URI prüfen: ",
+      loginText:
+        "Bitte einloggen, um Zugang zum Nextcloud Account zu ermöglichen.",
+    },
+  };
+
+    if (!(lang in translations)){
+      lang = "en";
+  }
+  if (!key in translations[lang]){
+      return "translation error";
+  }
+  return translations[lang][key];
+}
